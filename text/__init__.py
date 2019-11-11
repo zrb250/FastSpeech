315fa343625b5d659ff945f40992bc8d4f98341d
zhuribing <zhuribing@vipkid.com.cn>
2019-11-11 23:29:03 +0800

    modify text_to_sequence

--- text/__init__.py
+++ text/__init__.py
@@ -28,15 +28,17 @@ def text_to_sequence(text, cleaner_names):
     sequence = []
 
     # Check for curly braces and treat their contents as ARPAbet:
-    while len(text):
-        m = _curly_re.match(text)
-        if not m:
-            sequence += _symbols_to_sequence(_clean_text(text, cleaner_names))
-            break
-        sequence += _symbols_to_sequence(
-            _clean_text(m.group(1), cleaner_names))
-        sequence += _arpabet_to_sequence(m.group(2))
-        text = m.group(3)
+    if (len(text) > 0):
+        sequence = _arpabet_to_sequence(text)
+    #while len(text):
+        #m = _curly_re.match(text)
+        #if not m:
+        #    sequence += _symbols_to_sequence(_clean_text(text, cleaner_names))
+        #    break
+        #sequence += _symbols_to_sequence(
+        #    _clean_text(m.group(1), cleaner_names))
+        #sequence += _arpabet_to_sequence(m.group(2))
+        #text = m.group(3)
 
     return sequence
 
